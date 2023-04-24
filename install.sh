7e4f193d171b621a2c910d16051d0ac276128845
Denis Borovikov <43751473+borovikovd@users.noreply.github.com>
2023-04-24 16:22:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 mkdir -p $HOME/.tdk/bin
 
 # Fetch the latest JAR file
-LATEST_JAR_URL=$(curl -s https://synthesizedio.jfrog.io/artifactory/tdk-public/ | grep -o 'href="[^"]*.jar"' | sed 's/href="//;s/"//' | sort -V | tail -n 1)
-curl -L -o $HOME/.tdk/latest.jar "https://synthesizedio.jfrog.io/artifactory/tdk-public/$LATEST_JAR_URL"
+LATEST_JAR_URL=$(curl -s https://docs.synthesized.io/tdk/latest/yUJiBe6ai5DK05Zh2ttJ/download.html | grep -o 'https://synthesizedio.jfrog.io/artifactory/tdk-public/[^"]*.jar' | head -n 1)
+curl -L -o $HOME/.tdk/latest.jar "$LATEST_JAR_URL"
 
 # Create a wrapper shell script
 cat > $HOME/.tdk/bin/tdk << EOF
@@ -19,4 +19,3 @@ chmod +x $HOME/.tdk/bin/tdk
 echo "TDK installation is complete. To use it, add TDK to your path with:"
 echo 'export PATH=$HOME/.tdk/bin:$PATH'
 echo "You can add this line to your shell's profile file (e.g., .bashrc, .zshrc, .bash_profile, or .profile) to make the change permanent."
-

9c963fde07390ba2e48094343371a155c8e2f27f
Alex Bichuk <aliksbright@gmail.com>
2025-05-09 15:39:06 +0100

    v1.97.0 fix

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mkdir -p $HOME/.tdk/bin
 # FIXME: select latest version automatically 
 # LATEST_JAR_URL=$(curl -s https://synthesizedio.jfrog.io/artifactory/tdk-public/ | grep -o 'href="[^"]*.jar"' | sed 's/href="//;s/"//' | grep -E '_v[0-9]+\.[0-9]+\.[0-9]+\.jar$' | sort -t. -k2,2n -k3,3n -k4,4n | tail -n 1)
 LATEST_JAR_URL="synthesized_tdk_v1.97.0.jar"
-curl -L -o $HOME/.tdk/latest.jar "https://governor-static.synthesized.io/jars-public/synthesized_tdk_v1.97.0.jar?_gl=1*l4s1v*_gcl_au*MjA5MjAzNDc5NS4xNzQyODM0NTcz/$LATEST_JAR_URL"
+curl -L -o $HOME/.tdk/latest.jar "https://governor-static.synthesized.io/jars-public/$LATEST_JAR_URL"
 
 # Create a wrapper shell script
 cat > $HOME/.tdk/bin/tdk << EOF

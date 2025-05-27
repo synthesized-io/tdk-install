2068c1ebd4b68d524d475f568403a0ae5d6a2e2c
anastasiia <stepachevanastya@gmail.com>
2025-05-27 20:57:04 +0100

    [#6308] tdk latest/specified jar

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 # Create necessary directories
 mkdir -p $HOME/.tdk/bin
 
-# Fetch the latest JAR file
-LATEST_JAR_URL="synthesized_tdk_latest.jar"
-curl -L -o $HOME/.tdk/latest.jar "https://governor-static.synthesized.io/jars-public/$LATEST_JAR_URL"
+# Fetch the latest/specified JAR file
+TDK_VERSION=${TDK_VERSION:-"latest"}
+TDK_JAR="synthesized_tdk_${TDK_VERSION}.jar"
+curl -L -o $HOME/.tdk/latest.jar "https://governor-static.synthesized.io/jars-public/$TDK_JAR"
 
 # Create a wrapper shell script
 cat > $HOME/.tdk/bin/tdk << EOF
